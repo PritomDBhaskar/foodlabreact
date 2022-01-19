5001d0417508416acd09f6482aa2a96ae882e24f
PritomBhaskar <bhaskardebpritom@gmail.com>
2022-01-19 21:40:48 +0600

    api done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setdata] = useState({});
+  useEffect(() => {
+    fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=")
+      .then((res) => res.json())
+      .then((res) => {
+        setdata(res.meals);
+      });
+  }, []);
+  console.log(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="input-group mt-3">
+              <input type="text" className="form-control" />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          {data.map((item) => {
+            return (
+              <>
+                <div className="col-md-4">
+                  <div className="card m-4 border border-primary border-3">
+                    <img className="card-img-top" src={item.strMealThumb}></img>
+                    <h5 className="card-title m-3 text-center">
+                      {item.strMeal}
+                    </h5>
+                    <h6 className="card-subtitle m-3 text-muted text-center">
+                      {item.strCategory}
+                    </h6>
+                    <h6 className="card-subtitle m-3 text-muted text-center">
+                      {item.Area}
+                    </h6>
+                    <p className="card-text m-3 text-center">
+                      {item.strInstructions}
+                    </p>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 }
 
